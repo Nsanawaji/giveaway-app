08d91ba58b3f471a0fb8b378f5ee347e7eeeebbf
Nsanawaji <nsanawajiadasi@gmail.com>
2024-05-09 17:27:21 +0100

    Adding the BaseEntity class

--- src/Entities/base.entity.ts
+++ src/Entities/base.entity.ts
@@ -1,9 +1,11 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { timeStamp } from 'console';
 
 @Entity()
 export class BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  
+  @CreateDateColumn()
+  createdDate: Date;
 }
