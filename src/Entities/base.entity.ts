af5149fdcc6c38f1ab5e738a7c373a7bd0755046
Nsanawaji <nsanawajiadasi@gmail.com>
2024-05-09 17:24:19 +0100

    Adding the base and user Entities

---
+++ src/Entities/base.entity.ts
@@ -0,0 +1,9 @@
+import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  
+}
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,16 +1,25 @@
-import { Module } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
-
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRootAsync({
-            useFactory: async (
-                configService: ConfigService,
-            ): Promise<TypeOrmModuleOptions> => ({
-                
-            })
-        })
-    ]
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<TypeOrmModuleOptions> => ({
+        type: 'mysql',
+        host: configService.getOrThrow('DB_HOST'),
+        port: configService.getOrThrow('DB_PORT'),
+        username: configService.getOrThrow('DB_USER,'),
+        password: configService.getOrThrow('DB_PASSWORD'),
+        database: configService.getOrThrow('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
+export class DatabaseModule {}
